f556bf5caf7a64fa346fe952b43c3943df9d1410
Peter Magdy <petermagdyiskander@gmail.com>
2025-02-27 21:59:32 +0200

    [INIT] init commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PlayersComponent } from './features/players/players.component';
+import { TopElevenComponent } from './features/top-eleven/top-eleven.component';
+import { FormationComponent } from './features/formation/formation.component';
+import { PerformanceComponent } from './features/performance/performance.component';
+import { TrainingComponent } from './features/training/training.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'players', component: PlayersComponent },
+  { path: 'training', component: TrainingComponent },
+  { path: 'performance', component: PerformanceComponent },
+  { path: 'formation', component: FormationComponent },
+  { path: 'top-eleven', component: TopElevenComponent },
+];
---
+++ src/app/features/top-eleven/top-eleven.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-eleven',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-eleven.component.html',
+  styleUrl: './top-eleven.component.scss'
+})
+export class TopElevenComponent {
+
+}
